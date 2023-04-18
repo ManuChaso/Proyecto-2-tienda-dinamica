6c2878c20d7d5d553ac7f42c7e05ff336d80fd3a
ManuChaso <manuc.chaso@gmail.com>
2023-04-18 20:56:24 +0200

    Primer commit

---
+++ dataBase.js
@@ -0,0 +1,144 @@
+const DataBase = [
+    {
+        "seller": "marvel",
+        "price": 1500,
+        "name": "Iron Man Mark XLVII",
+        "image": "./assets/product-images/iron-man.jpg",
+        "description": "Figura de Iron Man con el diseño del Mark XLVII de la película 'Spider-Man: Homecoming'. Con 28 puntos de articulación, la figura cuenta con detalles metálicos en los brazos, piernas y pecho. Incluye accesorios como efectos de explosión y manos intercambiables."
+      },
+      {
+        "seller": "marvel",
+        "price": 1200,
+        "name": "Capitán América",
+        "image": "./assets/product-images/capitan-america.jpg",
+        "description": "Figura del Capitán América con su icónico traje y escudo. Cuenta con 30 puntos de articulación y detalles de pintura realistas. El escudo es desmontable y puede ser usado por la figura o sujetado a la espalda."
+      },
+      {
+        "seller": "marvel",
+        "price": 1000,
+        "name": "Spider-Man (Advanced Suit)",
+        "image": "./assets/product-images/spider-man.jpg",
+        "description": "Figura de Spider-Man con su traje 'Advanced Suit' de los videojuegos de PS4. Cuenta con 30 puntos de articulación y detalles de pintura realistas. Incluye accesorios como efectos de telaraña y manos intercambiables."
+      },
+      {
+        "seller": "marvel",
+        "price": 900,
+        "name": "Hulk",
+        "image": "./assets/product-images/hulk.jpg",
+        "description": "Figura de Hulk con su aspecto del Universo Cinematográfico de Marvel. Cuenta con 20 puntos de articulación y detalles de pintura realistas. La figura incluye un par de manos intercambiables."
+      },
+      {
+        "seller": "marvel",
+        "price": 800,
+        "name": "Thor",
+        "image": "./assets/product-images/thor.jpg",
+        "description": "Figura de Thor con su aspecto del Universo Cinematográfico de Marvel. Cuenta con 30 puntos de articulación y detalles de pintura realistas. Incluye accesorios como su martillo Mjolnir y manos intercambiables."
+      },
+      {
+        "seller": "dc",
+        "price": 1300,
+        "name": "Batman (Detective Comics #1000)",
+        "image": "./assets/product-images/batman.jpg",
+        "description": "Figura de Batman con su traje de la historieta 'Detective Comics #1000'. La figura cuenta con 22 puntos de articulación y detalles de pintura realistas. Incluye accesorios como manos intercambiables y un batarang."
+      },
+      {
+        "seller": "dc",
+        "price": 1100,
+        "name": "Superman",
+        "image": "./assets/product-images/superman.jpg",
+        "description": "Figura de Superman con su icónico traje y capa. La figura cuenta con 22 puntos de articulación y detalles de pintura realistas. Incluye accesorios como manos intercambiables y un par de efectos de visión calorífica."
+      },
+      {
+        "seller": "dc",
+        "price": 900,
+        "name": "Wonder Woman",
+        "image": "./assets/product-images/wonder-woman.jpg",
+        "description": "Figura de Wonder Woman con su traje clásico. La figura cuenta con 22 puntos de articulación y detalles de pintura realistas. Incluye accesorios como una espada y un escudo."
+      },
+      {
+        "seller": "dc",
+        "price": 800,
+        "name": "Flash",
+        "image": "./assets/product-images/flash.jpg",
+        "description": "Figura de Flash con su traje rojo y dorado. La figura cuenta con 22 puntos de articulación y detalles de pintura realistas. Incluye accesorios como manos intercambiables y un efecto de velocidad."
+      },
+      {
+        "seller": "dc",
+        "price": 700,
+        "name": "Joker",
+        "image": "./assets/product-images/joker.jpg",
+        "description": "Figura del Joker con su traje morado y verde. La figura cuenta con 22 puntos de articulación y detalles de pintura realistas. Incluye accesorios como una pistola y un cuchillo."
+      },
+      {
+        "seller": "one_piece",
+        "price": 1000,
+        "name": "Monkey D. Luffy (Gear Fourth)",
+        "image": "./assets/product-images/luffy.jpg",
+        "description": "Figura de Monkey D. Luffy en su forma Gear Fourth. La figura cuenta con 18 puntos de articulación y detalles de pintura realistas. Incluye accesorios como manos intercambiables y un efecto de haki del rey."
+      },
+      {
+        "seller": "one_piece",
+        "price": 900,
+        "name": "Roronoa Zoro",
+        "image": "./assets/product-images/zoro.jpg",
+        "description": "Figura de Roronoa Zoro con sus espadas. La figura cuenta con 18 puntos de articulación y detalles de pintura realistas. Incluye accesorios como manos intercambiables y efectos de cortes enemigos."
+      },
+      {
+        "seller": "one_piece",
+        "price": 800,
+        "name": "Sanji",
+        "image": "./assets/product-images/sanji.jpg",
+        "description": "Figura de Sanji con su traje negro. La figura cuenta con 18 puntos de articulación y detalles de pintura realistas. Incluye accesorios como manos intercambiables y un efecto de humo para su ataque Diable Jambe."
+      },
+      {
+        "seller": "one_piece",
+        "price": 700,
+        "name": "Nami",
+        "image": "./assets/product-images/nami.jpg",
+        "description": "Figura de Nami con su traje azul y su bastón. La figura cuenta con 18 puntos de articulación y detalles de pintura realistas. Incluye accesorios como manos intercambiables y una carta climática."
+      },
+      {
+        "seller": "one_piece",
+        "price": 600,
+        "name": "Tony Tony Chopper",
+        "image": "./assets/product-images/chopper.jpg",
+        "description": "Figura de Tony Tony Chopper en su forma de reno. La figura cuenta con 18 puntos de articulación y detalles de pintura realistas. Incluye accesorios como manos intercambiables y un efecto de su ataque Horn Point."
+      },
+      {
+        "seller": "attack_on_titan",
+        "price": 1200,
+        "name": "Eren Jaeger (Titan Form)",
+        "image": "./assets/product-images/eren.jpg",
+        "description": "Figura de Eren Jaeger en su forma de titán. La figura cuenta con 18 puntos de articulación y detalles de pintura realistas. Incluye accesorios como manos intercambiables y un efecto de humo para su transformación."
+      },
+      {
+        "seller": "attack_on_titan",
+        "price": 1100,
+        "name": "Mikasa Ackerman",
+        "image": "./assets/product-images/mikasa.jpg",
+        "description": "Figura de Mikasa Ackerman con su equipo de maniobras tridimensionales. La figura cuenta con 18 puntos de articulación y detalles de pintura realistas. Incluye accesorios como manos intercambiables y efectos de corte enemigo."
+      },
+      {
+        "seller": "attack_on_titan",
+        "price": 1000,
+        "name": "Levi Ackerman",
+        "image": "./assets/product-images/levi.jpg",
+        "description": "Figura de Levi Ackerman con su equipo de maniobras tridimensionales. La figura cuenta con 18 puntos de articulación y detalles de pintura realistas. Incluye accesorios como manos intercambiables y efectos de corte enemigo."
+      },
+      {
+        "seller": "attack_on_titan",
+        "price": 900,
+        "name": "Erwin Smith",
+        "image": "./assets/product-images/erwin.jpg",
+        "description": "Figura de Erwin Smith con su equipo de maniobras tridimensionales. La figura cuenta con 18 puntos de articulación y detalles de pintura realistas. Incluye accesorios como manos intercambiables y efectos de orden."
+      },
+      {
+        "seller": "attack_on_titan",
+        "price": 800,
+        "name": "Historia Reiss",
+        "image": "./assets/product-images/historia.jpg",
+        "description": "Figura de Historia Reiss con su uniforme militar. La figura cuenta con 18 puntos de articulación y detalles de pintura realistas. Incluye accesorios como manos intercambiables y un libro de historia."
+      }
+];
+
+export {DataBase};
---
+++ main.js
@@ -0,0 +1,79 @@
+//Import de la base de datos desde un archivo de js externo
+import {DataBase} from './dataBase.js';
+
+//plantilla de los productos que se mostrarán en el DOM
+const productTemplate = (product) => {
+    return `
+        <div class="product">
+            <img src="${product.image}" alt="Imagen del producto"/>
+            <p class="product-seller">${product.seller}</p>
+            <p class="product-name">${product.name}</p>
+            <p class="product-price">${product.price}€</p>
+            <div class="product-description">
+                <p>DESCRIPCIÓN:</p>
+                <p>${product.description}</p>
+            </div>
+        </div>
+    `
+}
+
+//Elementos del DOM a manupular
+const searchFilter = document.querySelector('.search');
+const priceFilter = document.querySelector('.price');
+const sellerFilter = document.querySelector('.seller');
+const submit = document.querySelector('.submit');
+const wipeFilter = document.querySelector('.wipe-filter');
+const productList = document.querySelector('.product-list');
+
+//eventos
+submit.addEventListener('click', handleSubmmit);
+wipeFilter.addEventListener('click', showProducts);
+searchFilter.addEventListener('keydown', pressEnter);
+priceFilter.addEventListener('keydown', pressEnter);
+sellerFilter.addEventListener('change', handleSubmmit)
+
+
+//función que llama a la función habndleSubmit en caso de aprentar "Enter" cuando el foco este en "precio" o "Nombre"
+function pressEnter(e){
+    if(e.key === "Enter") handleSubmmit();
+}
+
+//Función que se encara tanto de mostrar todos los productos por defecto como de borrar los filtros cuando se presiona el boton de "borrar filtros"
+function showProducts(){
+    searchFilter.value = "";
+    priceFilter.value = "";
+    sellerFilter.value = "all";
+
+    productList.innerHTML = '';
+    DataBase.forEach(element =>{
+        productList.innerHTML += productTemplate(element);
+    });
+}
+
+//Función que se encarga de recoger los inputs y filtrar los productos
+function handleSubmmit() {
+    productList.innerHTML = '';
+    
+    const name = searchFilter.value.toLowerCase();
+    const price = priceFilter.value;
+    const seller = sellerFilter.value;
+    
+
+    DataBase.forEach(element =>{
+
+        let handleElementName = element.name.toLowerCase().replace(/ /g, "");
+        let handleName = name.replace(/ /g, "");
+
+
+        const checkName = name ? handleElementName.includes(handleName) : true;
+        const checkPrice = price ? Number(element.price) <= Number(price) : true;
+        const checkSeller = seller !== "all" ? element.seller === seller : true;
+
+        if(checkName && checkPrice && checkSeller){
+            productList.innerHTML += productTemplate(element);
+        }
+    })
+}
+
+//Llamada a la funcion showProducts para que se muestre la lista completa de productos al cargar la pagina
+showProducts();
